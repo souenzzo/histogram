68cc4c651a5c55a43eeb3df5d320ad9306c74ee3
Enzzo Cavallo <souenzzo@gmail.com>
2018-11-24 10:09:16 -0200

    WIP

--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,134 @@
-use std::ops::Add;
-use std::ops::Div;
+extern crate num_traits;
+extern crate core;
+
 use std::collections::BTreeMap;
+use std::ops;
+use std::cmp::Ordering;
+use std::fmt;
 
-trait Number: Copy {
-    fn one() -> Self;
-    fn max(self, a: Self) -> Self;
-    fn min(self, a: Self) -> Self;
-    fn add(self, a: Self) -> Self;
-    fn sorted_merge(a: Vec<Self>, b: Vec<Self>) -> Self;
-}
-
-trait IHistogram<T> {
-    fn new(_: T) -> Box<Self>;
-    fn combine(self, _: Box<Self>) -> Box<Self>;
-}
-
-struct Histogram<T> {
+pub struct Histogram<T> {
     max: T,
     min: T,
     size: T,
-    sorted_values: Vec<T>,
+    sorted_values: BTreeMap<T, T>,
 }
 
-impl<T> IHistogram<T> for Histogram<T> where T: HistValue {
-    fn new(a: T) -> Box<Self> {
-        return Box::new(
-            Histogram {
-                max: a,
-                min: a,
-                size: HistValue::one(),
-                sorted_values: vec!(a),
-            }
-        );
+pub trait Num: num_traits::One + num_traits::Zero + ops::Add + Ord + Clone + fmt::Display {}
+
+impl<T> Histogram<T> where T: Num {
+    fn new(a: T) -> Self {
+        let mut sorted_values = BTreeMap::new();
+        sorted_values.insert(T::clone(&a), T::one());
+        return Self {
+            max: T::clone(&a),
+            min: T::clone(&a),
+            size: T::one(),
+            sorted_values: sorted_values,
+        };
     }
 
-    fn combine(self, a: Box<Self>) -> Box<Self> {
-        return Box::new(
-            Histogram {
-                max: T::max(self.max, a.max),
-                min: T::min(self.min, a.min),
-                size: T::add(self.size, a.size),
-                sorted_values: T::sorted_merge(
-                    self.sorted_values,
-                    a.sorted_values),
+
+    fn from_vec(data: Vec<T>) -> Option<Self> {
+        let maybe_data = data.split_first();
+        match maybe_data {
+            None => {
+                None
             }
-        );
+            Some((v0, vs)) => {
+                Some(vs.iter().fold(Self::new(T::clone(&v0)),
+                                    |acc, v|
+                                        acc.combine(Self::new(T::clone(&v)))))
+            }
+        }
+    }
+
+    fn combine(self, a: Self) -> Self {
+        let mut sorted_values = BTreeMap::new();
+        sorted_values.clone_from(&self.sorted_values);
+        for (i, v) in a.sorted_values.iter() {
+            let actual_v = self.sorted_values.get(i);
+            match actual_v {
+                None => {
+                    sorted_values.insert(T::clone(&i), T::clone(&v));
+                }
+                Some(x) => {
+                    sorted_values.insert(T::clone(&i),
+                                         T::add(T::clone(&v), T::clone(&x)),
+                    );
+                }
+            };
+        }
+        return Self {
+            max: match T::cmp(&self.max, &a.max) {
+                Ordering::Equal => {
+                    self.max
+                }
+                Ordering::Less => {
+                    a.max
+                }
+                Ordering::Greater => {
+                    self.max
+                }
+            },
+            min: match T::cmp(&self.min, &a.min) {
+                Ordering::Equal => {
+                    self.min
+                }
+                Ordering::Less => {
+                    self.min
+                }
+                Ordering::Greater => {
+                    a.min
+                }
+            },
+            size: T::add(self.size, a.size),
+            sorted_values: sorted_values,
+        };
+    }
+
+    fn normalize_by_min_plus_interval(self, i: T) -> Self {
+        unimplemented!()
     }
 }
+
+impl<T> PartialEq for Histogram<T> where T: Num {
+    fn eq(&self, other: &Self) -> bool {
+        return self.sorted_values == other.sorted_values;
+    }
+}
+
 
 fn main() {
-    let a = Average::new(5.0);
-    let b = Average::new(3.0);
-    let c = a.combine(b);
-    let x = c.result();
-    println!("Hello, world! {} ", x);
+    println!("Hello, world! ");
+}
+
+#[cfg(test)]
+mod test {
+    extern crate core;
+
+    use Histogram;
+    use Num;
+    use std::fmt;
+
+    impl Num for i64 {}
+
+    impl<T> fmt::Debug for Histogram<T> where T: Num {
+        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+            let content = self.sorted_values.iter().map(|e| {
+                let (k, v) = e;
+                // TODO: Print as vec!() form
+                return format!("{} {}", k, v);
+            });
+            write!(f, "Histogram::from_vec (vec!({}))",
+                   content.fold(String::new(), |acc, x| format!("{} {},", acc, x)))
+        }
+    }
+
+    #[test]
+    fn test_01() {
+        assert_eq!(
+            Some(Histogram::new(1)),
+            Histogram::from_vec(vec!(1)),
+        )
+    }
 }
